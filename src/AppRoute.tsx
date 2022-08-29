b0cb86ebd2083c895d4bb9487893475a596acc56
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-08-29 20:55:39 +0200

    Add error boundaries

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import ErrorBoundary from './components/ErrorBoundaries/ErrorBoundaries';
 import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
 import { Dashboard } from './pages/Dashboard/Dashboard';
 
 export function AppRoute() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<PrivateRoute>
-							<Dashboard />
-						</PrivateRoute>
-					}
-				/>
-			</Routes>
-		</BrowserRouter>
+		<ErrorBoundary>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<PrivateRoute>
+								<Dashboard />
+							</PrivateRoute>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</ErrorBoundary>
 	);
 }
