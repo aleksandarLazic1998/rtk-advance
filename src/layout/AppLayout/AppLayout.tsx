afecbd7fa4cf77a010ec417a9806ffd28d4af169
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-09-07 21:34:26 +0200

    Add basic styles for navbar

--- src/layout/AppLayout/AppLayout.tsx
+++ src/layout/AppLayout/AppLayout.tsx
@@ -1,9 +1,22 @@
 import React, { ReactNode } from 'react';
 
+import { Navbar } from '../../components/Navbar/Navbar';
+import { Sidebar } from '../../components/Sidebar/Sidebar';
+
+import './AppLayout.css';
+
 interface IProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 export function AppLayout({ children }: IProps) {
-  return <div>{children}</div>;
+	return (
+		<div className="app-layout">
+			<Sidebar />
+			<div className="main-component">
+				<Navbar />
+				<div className="main-component__section">{children} </div>
+			</div>
+		</div>
+	);
 }
