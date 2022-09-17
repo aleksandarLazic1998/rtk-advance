fd36e522395ddf8362bd5507d71521c39e65cc8f
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-09-17 12:36:49 +0200

    Add toast notifications for macro errors

--- src/store/middlewares/rtkQueryErrorLogger.ts
+++ src/store/middlewares/rtkQueryErrorLogger.ts
@@ -1,5 +1,6 @@
 import { isRejectedWithValue, PayloadAction } from '@reduxjs/toolkit';
 import type { Middleware } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 export const rtkQueryErrorLogger: Middleware =
 	() => (next) => (action: PayloadAction<{ originalStatus: number }>) => {
@@ -10,12 +11,14 @@ export const rtkQueryErrorLogger: Middleware =
 				action.payload.originalStatus === 401 && retried;
 			if (isFailedWithRetry) {
 				// Logout user
+				toast.error('You are unauthorized and will be logged out.');
 			}
 
 			const isFailedFirstTime =
-				action.payload.originalStatus === 401 && !retried;
+				action.payload.originalStatus === 404 && !retried;
 			if (isFailedFirstTime) {
 				retried = true;
+				toast.error('You are unauthorized');
 
 				try {
 					// do refresh token logic and in success refresh token pass the same request to retry
